40cb4867df6f9fa195b1983d2d40d19d081b0755
Niklas Lampén <niklas@emptymonkey.com>
2024-03-08 18:08:37 +0200

    Initial Commit

---
+++ Sim Racing Telemetry/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sim Racing Telemetry
+//
+//  Created by Niklas Lampen on 8.3.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
